5b62c44064c0bcca3881971237906e19dceff7e2
DanilStan <stanislavov.danik@gmail.com>
2022-11-04 22:47:53 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,3 @@
-<<<<<<< Updated upstream
-import { Component } from 'react';
-import { FeedbackOptions } from './FeedbackOptions';
-import { Section } from './Section';
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  render() {
-    return (
-      <Section title={'Please leave feedback'}>
-        <FeedbackOptions options={this.state} />
-        <h2>Statistics</h2>
-
-        <ul>
-          <li>
-            <p>Good</p>
-          </li>
-          <li>
-            <p>Neutral</p>
-          </li>
-          <li>
-            <p>Bad</p>
-          </li>
-        </ul>
-      </Section>
-    );
-  }
-}
-=======
 import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 
@@ -60,4 +27,3 @@ export const App = () => {
     </>
   );
 };
->>>>>>> Stashed changes
