95aefbb4f021b5b015b6947472bf167fd43df1d9
DanilStan <stanislavov.danik@gmail.com>
2022-11-04 18:52:19 +0200

    Update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { FeedbackOptions } from './FeedbackOptions';
+import { Section } from './Section';
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions options={this.state} />
+        <h2>Statistics</h2>
+
+        <ul>
+          <li>
+            <p>Good</p>
+          </li>
+          <li>
+            <p>Neutral</p>
+          </li>
+          <li>
+            <p>Bad</p>
+          </li>
+        </ul>
+      </Section>
+    );
+  }
+}
